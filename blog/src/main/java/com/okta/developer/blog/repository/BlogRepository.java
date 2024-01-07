d21f38e1f275d3387277809037fa03f87da87c26
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-01-08 00:17:20 +0700

    Initial version of blog generated by generator-jhipster@8.1.0

---
+++ blog/src/main/java/com/okta/developer/blog/repository/BlogRepository.java
@@ -0,0 +1,12 @@
+package com.okta.developer.blog.repository;
+
+import com.okta.developer.blog.domain.Blog;
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data Neo4j reactive repository for the Blog entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BlogRepository extends ReactiveNeo4jRepository<Blog, String> {}
